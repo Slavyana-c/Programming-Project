83431010e26a5be457005fa5a1d2973d8936b5b3
Slavyana Chervenkondeva [sc17sdc] <sc17sdc@comp-pc6132.leeds.ac.uk>
2018-02-21 18:04:13 +0000

    Moved files into a directory.

---
+++ 01_Project/buildTree.h
@@ -0,0 +1,7 @@
+#include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+
+Node *makeNode( double x, double y, int level );
+void makeChildren(Node *parent);
+void growTree( Node *node, int maxLvl );
---
+++ 01_Project/writeTree.h
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+
+void writeNode( FILE *fp, Node *node );
+void writeNode( FILE *fp, Node *node );
+void printOut( FILE *fp, Node *node );
+void destroyTree(Node *node);
