b7cf505331791a1aa4e82dfbb887e992626ba6e3
Slavyana Chervenkondeva [sc17sdc] <sc17sdc@comp-pc3005.leeds.ac.uk>
2018-04-18 12:31:38 +0100

    Changes in game.h

--- Roger_Stevens/game.h
+++ Roger_Stevens/game.h
@@ -6,5 +6,6 @@
 void movePlayer(int map[][MAP_WIDTH_BLOCKS], SDL_Rect *pos, int direction);
 void renderText(SDL_Renderer *renderer);
 int play(SDL_Renderer *renderer, char fileName[]);
+void savedMessage(SDL_Renderer *renderer);
 
 #endif
