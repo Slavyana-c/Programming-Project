98a327543b213267e6f699c4ea24696f33ac2df6
Slavyana Chervenkondeva [sc17sdc] <sc17sdc@comp-pc3006.leeds.ac.uk>
2018-04-19 11:01:06 +0100

    Added lose screen

--- Roger_Stevens/endgame.c
+++ Roger_Stevens/endgame.c
@@ -38,5 +38,33 @@ void ShowWin(SDL_Renderer *renderer)
 
 void ShowLose(SDL_Renderer *renderer)
 {
+  SDL_Texture *lose = NULL;
+  SDL_Event event;
+
+  lose = IMG_LoadTexture(renderer, "images/lose.jpg");
+
+  // Event loop
+  while (GAME_MODE != MENU)
+  {
+      SDL_WaitEvent(&event);
+      switch(event.type)
+      {
+          // Close the widow
+          case SDL_QUIT:
+              GAME_MODE = QUIT;
+              break;
+
+          // Go to menu
+          case SDL_KEYDOWN:
+              GAME_MODE = MENU;
+              break;
+      }
+
+      // Render the screen
+      SDL_RenderClear(renderer);
+      SDL_RenderCopy(renderer, lose, NULL, NULL);
+      SDL_RenderPresent(renderer);
+  }
+  SDL_DestroyTexture(lose);
 
 }
